79d0aa7961acc807dfeef55ad1bdbb45ee20b8df
Garrett Gunnell <gunnellg@oregonstate.edu>
2022-04-08 17:25:07 -0700

    move editor to tonemapper.cs

--- Assets/Scripts/Tonemapper.cs
+++ Assets/Scripts/Tonemapper.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Tonemapper : MonoBehaviour {
     public Shader tonemapperShader;
@@ -19,5 +20,24 @@ public class Tonemapper : MonoBehaviour {
 
     void OnRenderImage(RenderTexture source, RenderTexture destination) {
         Graphics.Blit(source, destination, tonemapperMat, (int)toneMapper);
+    }
+}
+
+[CustomEditor(typeof(Tonemapper))]
+[CanEditMultipleObjects]
+public class TonemapperEditor : Editor {
+    SerializedProperty tonemapperShader;
+    SerializedProperty toneMapper;
+
+    void OnEnable() {
+        tonemapperShader = serializedObject.FindProperty("tonemapperShader");
+        toneMapper = serializedObject.FindProperty("toneMapper");
+    }
+
+    public override void OnInspectorGUI() {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(tonemapperShader);
+        EditorGUILayout.PropertyField(toneMapper);
+        serializedObject.ApplyModifiedProperties();
     }
 }
