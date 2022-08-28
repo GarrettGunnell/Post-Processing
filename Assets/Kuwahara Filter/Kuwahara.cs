9a366ef183801d447c894ce2482fe603532a1fbf
Garrett Gunnell <gunnellg@oregonstate.edu>
2022-08-28 15:50:09 -0700

    Add files for kuwahara filter implementation

---
+++ Assets/Kuwahara Filter/Kuwahara.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kuwahara : MonoBehaviour {
+    public Shader kuwaharaShader;
+    
+    [Range(1, 20)]
+    public int kernelSize = 1;
+
+    private Material kuwaharaMat;
+    
+    void Start() {
+        kuwaharaMat ??= new Material(kuwaharaShader);
+        kuwaharaMat.hideFlags = HideFlags.HideAndDontSave;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        kuwaharaMat.SetFloat("_KernelSize", kernelSize);
+        Graphics.Blit(source, destination, kuwaharaMat);
+    }
+}
